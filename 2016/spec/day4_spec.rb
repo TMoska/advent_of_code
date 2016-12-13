e94c38934dbe3a301093eff6eefd7fdda8db8068
Tomas MOSKA <tmoska@e2r9p1.42.fr>
2016-12-13 18:13:56 +0100

    add tests for day4

---
+++ 2016/spec/day4_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../day4/run.rb'
+RSpec.describe "Day 4" do
+	it "identifies complex real room" do
+		expect(is_real("aaaaa-bbb-z-y-x-123[abxyz]")).to eq true
+	end
+	it "identifies simple real room" do
+		expect(is_real("a-b-c-d-e-f-g-h-987[abcde]")).to eq true
+	end
+	it "identifies a real room" do
+		expect(is_real("not-a-real-room-404[oarel]")).to eq true
+	end
+	it "identifies a not real room" do
+		expect(is_real("totally-real-room-200[decoy]")).to eq false
+	end
+	if "returns a correct answer" do
+		expect(solve_d4_1("../../spec/test_inputs/d4/p1t1.txt").to eq 1514
+	end
+end
